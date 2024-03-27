090aaf3f29f1b87e3cf9d16d6b7df01223cb3b5f
22521618 <22521618@gm.uit.edu.vn>
2024-03-27 16:51:48 +0700

    Add project files.

---
+++ Movie_Web/Models/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Movie_Web.Models;
+
+public partial class Account
+{
+    public int AccountId { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Password { get; set; }
+
+    public bool Active { get; set; }
+
+    public string? FullName { get; set; }
+
+    public int? RoleId { get; set; }
+
+    public DateOnly? LastLogin { get; set; }
+
+    public DateOnly? CreateDate { get; set; }
+
+    public virtual ICollection<Rate> Rates { get; set; } = new List<Rate>();
+
+    public virtual Role? Role { get; set; }
+}
---
+++ Movie_Web/Program.cs
@@ -0,0 +1,60 @@
+using AspNetCoreHero.ToastNotification;
+using Microsoft.EntityFrameworkCore;
+using Movie_Web.Models;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace Movie_Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            builder.Services.AddNotyf(config =>
+            {
+                config.DurationInSeconds = 10;
+                config.IsDismissable = true;
+                config.Position = NotyfPosition.BottomRight;
+            });
+
+            var stringConnectDb = builder.Configuration.GetConnectionString("dbMovie");
+            builder.Services.AddDbContext<MoviesContext>(options => options.UseSqlServer(stringConnectDb));
+            builder.Services.AddSingleton<HtmlEncoder>(HtmlEncoder.Create(allowedRanges: new[] { UnicodeRanges.All }));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
